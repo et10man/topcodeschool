9370aee6a123a16c1e57749fafbb1220566d9c19
james-yl.tsai <jameslovee10@gmail.com>
2019-04-30 13:42:55 +0800

    add Concept_Problem_2016_03_05_6.vue

--- src/views/courses/apcs/archive/concept/2016-03-05/Concept_Problem_2016_03_05_6.vue
+++ src/views/courses/apcs/archive/concept/2016-03-05/Concept_Problem_2016_03_05_6.vue
@@ -1,11 +1,70 @@
 <template>
   <v-container>
-    <h1>Problem-6</h1>
+    <v-layout row wrap justify-center>
+      <v-flex xs5>
+        <v-img :src="image.description_1"></v-img>
+        <v-img :src="image.description_2"></v-img>
+      </v-flex>
+      <v-flex xs5>
+        <v-img :src="image.code"></v-img>
+      </v-flex>
+      <v-flex xs2></v-flex>
+      <v-flex xs5>
+        <v-radio-group v-model="answerGroup">
+          <v-radio
+            label="list[current].next = current ;"
+            color="error"
+            value="error_1"
+          ></v-radio>
+          <v-radio
+            label="list[current].next = list[list[current].next].next ;"
+            color="success"
+            value="success_2"
+          ></v-radio>
+          <v-radio
+            label="current = list[list[current].next].next ;"
+            color="error"
+            value="error_3"
+          ></v-radio>
+          <v-radio
+            label="list[list[current].next].next = list[current].next ;"
+            color="error"
+            value="error_4"
+          ></v-radio>
+        </v-radio-group>
+      </v-flex>
+      <v-flex xs5>
+        <v-btn color="info" @click="bShowAnswer = !bShowAnswer">{{
+          bShowAnswer ? "隱藏詳解" : "顯示詳解"
+        }}</v-btn>
+        <div v-if="bShowAnswer">
+          <h2>解答:</h2>
+          <div class="subheading">
+            <div>將current的next指向current的next的next</div>
+            <div>
+              顧answer = list[current].next = list[list[current].next].next ;
+            </div>
+          </div>
+        </div>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
-
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      date: "2016-03-05",
+      image: {
+        description_1: require("@/assets/problems/concept/2016-03-05/6_description_1.png"),
+        description_2: require("@/assets/problems/concept/2016-03-05/6_description_2.png"),
+        code: require("@/assets/problems/concept/2016-03-05/6_code.png")
+      },
+      answerGroup: [],
+      bShowAnswer: false
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
