0d4023e97af612d74fb70386c35e3e5c9e0b1374
james-yl.tsai <jameslovee10@gmail.com>
2019-05-02 16:39:21 +0800

    add Concept_Problem_2016_03_05_15.vue

--- src/views/courses/apcs/archive/concept/2016-03-05/Concept_Problem_2016_03_05_15.vue
+++ src/views/courses/apcs/archive/concept/2016-03-05/Concept_Problem_2016_03_05_15.vue
@@ -1,11 +1,51 @@
 <template>
   <v-container>
-    <h1>Problem-15</h1>
+    <v-layout row wrap justify-center>
+      <v-flex xs5>
+        <v-img :src="image.description"></v-img>
+      </v-flex>
+      <v-flex xs5>
+        <v-img :src="image.code"></v-img>
+      </v-flex>
+      <v-flex xs2></v-flex>
+      <v-flex xs3>
+        <v-radio-group v-model="answerGroup">
+          <v-radio label="16" color="success" value="success_1"></v-radio>
+          <v-radio label="22" color="error" value="error_2"></v-radio>
+          <v-radio label="11" color="error" value="error_3"></v-radio>
+          <v-radio label="15" color="error" value="error_4"></v-radio>
+        </v-radio-group>
+      </v-flex>
+      <v-flex xs7>
+        <v-btn color="info" @click="bShowAnswer = !bShowAnswer">
+          {{ bShowAnswer ? "隱藏詳解" : "顯示詳解" }}
+        </v-btn>
+        <div v-if="bShowAnswer">
+          <h2>解答:</h2>
+          <div class="subheading">
+            <div>共印出16個數字</div>
+            <div>22->11->34->17->52->26->13->40->20->10->5->16->8->4->2->1</div>
+            <div>故answer=16</div>
+          </div>
+        </div>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
-
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      date: "2016-03-05",
+      image: {
+        description: require("@/assets/problems/concept/2016-03-05/15_description.png"),
+        code: require("@/assets/problems/concept/2016-03-05/15_code.png")
+      },
+      answerGroup: [],
+      bShowAnswer: false
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
