c93d8173f93ba43258212939c8c617bc3976dcf0
james-yl.tsai <jameslovee10@gmail.com>
2019-05-02 16:57:40 +0800

    add Concept_Problem_2016_03_05_18.vue

--- src/views/courses/apcs/archive/concept/2016-03-05/Concept_Problem_2016_03_05_18.vue
+++ src/views/courses/apcs/archive/concept/2016-03-05/Concept_Problem_2016_03_05_18.vue
@@ -1,11 +1,47 @@
 <template>
   <v-container>
-    <h1>Problem-18</h1>
+    <v-layout row wrap justify-center>
+      <v-flex xs5>
+        <v-img :src="image.description"></v-img>
+      </v-flex>
+      <v-flex xs7></v-flex>
+      <v-flex xs3>
+        <v-radio-group v-model="answerGroup">
+          <v-radio label="語法錯誤" color="success" value="success_1"></v-radio>
+          <v-radio label="語意錯誤" color="error" value="error_2"></v-radio>
+          <v-radio label="邏輯錯誤" color="error" value="error_3"></v-radio>
+          <v-radio label="以上皆是" color="error" value="error_4"></v-radio>
+        </v-radio-group>
+      </v-flex>
+      <v-flex xs7>
+        <v-btn color="info" @click="bShowAnswer = !bShowAnswer">{{
+          bShowAnswer ? "隱藏詳解" : "顯示詳解"
+        }}</v-btn>
+        <div v-if="bShowAnswer">
+          <h2>解答:</h2>
+          <div class="subheading">
+            <div>答案為語法錯誤 ex. else拼成esle</div>
+            <div>語法錯誤和邏輯錯誤編譯器沒辦法找到</div>
+            <div>故answer=語法錯誤</div>
+          </div>
+        </div>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
-
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      date: "2016-03-05",
+      image: {
+        description: require("@/assets/problems/concept/2016-03-05/18_description.png")
+      },
+      answerGroup: [],
+      bShowAnswer: false
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
