9e265bb956dc9385af96e5624fe3d4c51ef4ba39
james-yl.tsai <jameslovee10@gmail.com>
2019-05-03 02:00:46 +0800

    fix 24&25 order

--- src/views/courses/apcs/archive/concept/2016-03-05/Concept_Problem_2016_03_05_24.vue
+++ src/views/courses/apcs/archive/concept/2016-03-05/Concept_Problem_2016_03_05_24.vue
@@ -10,26 +10,10 @@
       <v-flex xs2></v-flex>
       <v-flex xs4>
         <v-radio-group v-model="answerGroup">
-          <v-radio
-            label="x + Mystery(x-1)"
-            color="error"
-            value="error_1"
-          ></v-radio>
-          <v-radio
-            label="x * Mystery(x-1)"
-            color="error"
-            value="error_2"
-          ></v-radio>
-          <v-radio
-            label="Mystery(x-2) + Mystery(x+2)"
-            color="error"
-            value="error_3"
-          ></v-radio>
-          <v-radio
-            label="Mystery(x-2) + Mystery(x-1)"
-            color="success"
-            value="success_4"
-          ></v-radio>
+          <v-radio label="6" color="error" value="error_1"></v-radio>
+          <v-radio label="11" color="error" value="error_2"></v-radio>
+          <v-radio label="13" color="success" value="success_3"></v-radio>
+          <v-radio label="14" color="error" value="error_4"></v-radio>
         </v-radio-group>
       </v-flex>
       <v-flex xs8>
@@ -39,52 +23,22 @@
         <div v-if="bShowAnswer">
           <h2>解答:</h2>
           <div class="subheading">
-            <div>(A) x + Mystery(x-1)</div>
-            <div>Mystery(9) = 9+Mystery(8)</div>
-            <div>Mystery(9) = 9+8+Mystery(7)</div>
-            <div>Mystery(9) = 9+8+7+Mystery(6)</div>
-            <div>Mystery(9) = 9+8+7+Mystery(6)</div>
-            <div>Mystery(9) = 9+8+7+6+Mystery(5)</div>
-            <div>Mystery(9) = 9+8+7+6+5+Mystery(4)</div>
-            <div>Mystery(9) = 9+8+7+6+5+4+Mystery(3)</div>
-            <div>Mystery(9) = 9+8+7+6+5+4+3+Mystery(2)</div>
-            <div>Mystery(9) = 9+8+7+6+5+4+3+2+Mystery(1)</div>
-            <div>Mystery(9) = 9+8+7+6+5+4+3+2+1 = 45</div>
-            <div>(B) x * Mystery(x-1)</div>
-            <div>Mystery(9) = 9*Mystery(8)</div>
-            <div>Mystery(9) = 9*8*Mystery(7)</div>
-            <div>Mystery(9) = 9*8*7*Mystery(6)</div>
-            <div>Mystery(9) = 9*8*7*Mystery(6)</div>
-            <div>Mystery(9) = 9*8*7*6*Mystery(5)</div>
-            <div>Mystery(9) = 9*8*7*6*5*Mystery(4)</div>
-            <div>Mystery(9) = 9*8*7*6*5*4*Mystery(3)</div>
-            <div>Mystery(9) = 9*8*7*6*5*4*3*Mystery(2)</div>
-            <div>Mystery(9) = 9*8*7*6*5*4*3*2*Mystery(1)</div>
-            <div>Mystery(9) = 9*8*7*6*5*4*3*2*1 = 362880</div>
-            <div>(C) Mystery(x-2) + Mystery(x+2)</div>
-            <div>(D) Mystery(x-2) + Mystery(x-1)</div>
-            <div>Mystery(9) = Mystery(7) + Mystery(8)</div>
-            <div>從Mystery(1)反推回去</div>
-            <div>Mystery(1) = 1</div>
-            <div>Mystery(2) = 1</div>
-            <div>Mystery(3) = 2</div>
-            <div>Mystery(4) = 3</div>
-            <div>Mystery(5) = 5</div>
-            <div>Mystery(6) = 8</div>
-            <div>Mystery(7) = 13</div>
-            <div>Mystery(8) = 21</div>
-            <div>Mystery(9) = 34</div>
-            <div>Mystery(10) = 55</div>
-            <div>Mystery(11) = 89</div>
-            <div>
-              (C) Mystery(x-2) + Mystery(x+2) = Mystery(7) + Mystery(11) = 13 +
-              89 = 102
-            </div>
-            <div>
-              (D) Mystery(x-2) + Mystery(x-1) = Mystery(7) + Mystery(8) = 13 +
-              21 = 34
-            </div>
-            <div>故answer = (D)</div>
+            <div>g(4)</div>
+            <div>for loop會進入三次</div>
+            <div>目前j = 0</div>
+            <div>j = 0 + f(1)</div>
+            <div>j = 0 + (1 + f(2))</div>
+            <div>j = 0 + 1 + (3 + f(3))</div>
+            <div>j = 0 + 1 + 3 + (1 + (f4))</div>
+            <div>j = 0 + 1 + 3 + 1 + (1) = 6</div>
+            <div>可以知道</div>
+            <div>f(2) = 3 + f(3) = 5</div>
+            <div>f(3) = 1 + f(4) = 2</div>
+            <div>目前j = 6</div>
+            <div>j = 6 + f(2) = 6 + 5 = 11</div>
+            <div>目前j = 11</div>
+            <div>j = j + f(3) = 11 + 2 = 13</div>
+            <div>故answer = 13</div>
           </div>
         </div>
       </v-flex>
@@ -97,8 +51,8 @@ export default {
     return {
       date: "2016-03-05",
       image: {
-        description: require("@/assets/problems/concept/2016-03-05/25_description.png"),
-        code: require("@/assets/problems/concept/2016-03-05/25_code.png")
+        description: require("@/assets/problems/concept/2016-03-05/24_description.png"),
+        code: require("@/assets/problems/concept/2016-03-05/24_code.png")
       },
       answerGroup: [],
       bShowAnswer: false
--- src/views/courses/apcs/archive/concept/2016-03-05/Concept_Problem_2016_03_05_25.vue
+++ src/views/courses/apcs/archive/concept/2016-03-05/Concept_Problem_2016_03_05_25.vue
@@ -1,11 +1,110 @@
 <template>
   <v-container>
-    <h1>Problem-25</h1>
+    <v-layout row wrap justify-center>
+      <v-flex xs5>
+        <v-img :src="image.description"></v-img>
+      </v-flex>
+      <v-flex xs5>
+        <v-img :src="image.code"></v-img>
+      </v-flex>
+      <v-flex xs2></v-flex>
+      <v-flex xs4>
+        <v-radio-group v-model="answerGroup">
+          <v-radio
+            label="x + Mystery(x-1)"
+            color="error"
+            value="error_1"
+          ></v-radio>
+          <v-radio
+            label="x * Mystery(x-1)"
+            color="error"
+            value="error_2"
+          ></v-radio>
+          <v-radio
+            label="Mystery(x-2) + Mystery(x+2)"
+            color="error"
+            value="error_3"
+          ></v-radio>
+          <v-radio
+            label="Mystery(x-2) + Mystery(x-1)"
+            color="success"
+            value="success_4"
+          ></v-radio>
+        </v-radio-group>
+      </v-flex>
+      <v-flex xs8>
+        <v-btn color="info" @click="bShowAnswer = !bShowAnswer">{{
+          bShowAnswer ? "隱藏詳解" : "顯示詳解"
+        }}</v-btn>
+        <div v-if="bShowAnswer">
+          <h2>解答:</h2>
+          <div class="subheading">
+            <div>(A) x + Mystery(x-1)</div>
+            <div>Mystery(9) = 9+Mystery(8)</div>
+            <div>Mystery(9) = 9+8+Mystery(7)</div>
+            <div>Mystery(9) = 9+8+7+Mystery(6)</div>
+            <div>Mystery(9) = 9+8+7+Mystery(6)</div>
+            <div>Mystery(9) = 9+8+7+6+Mystery(5)</div>
+            <div>Mystery(9) = 9+8+7+6+5+Mystery(4)</div>
+            <div>Mystery(9) = 9+8+7+6+5+4+Mystery(3)</div>
+            <div>Mystery(9) = 9+8+7+6+5+4+3+Mystery(2)</div>
+            <div>Mystery(9) = 9+8+7+6+5+4+3+2+Mystery(1)</div>
+            <div>Mystery(9) = 9+8+7+6+5+4+3+2+1 = 45</div>
+            <div>(B) x * Mystery(x-1)</div>
+            <div>Mystery(9) = 9*Mystery(8)</div>
+            <div>Mystery(9) = 9*8*Mystery(7)</div>
+            <div>Mystery(9) = 9*8*7*Mystery(6)</div>
+            <div>Mystery(9) = 9*8*7*Mystery(6)</div>
+            <div>Mystery(9) = 9*8*7*6*Mystery(5)</div>
+            <div>Mystery(9) = 9*8*7*6*5*Mystery(4)</div>
+            <div>Mystery(9) = 9*8*7*6*5*4*Mystery(3)</div>
+            <div>Mystery(9) = 9*8*7*6*5*4*3*Mystery(2)</div>
+            <div>Mystery(9) = 9*8*7*6*5*4*3*2*Mystery(1)</div>
+            <div>Mystery(9) = 9*8*7*6*5*4*3*2*1 = 362880</div>
+            <div>(C) Mystery(x-2) + Mystery(x+2)</div>
+            <div>(D) Mystery(x-2) + Mystery(x-1)</div>
+            <div>Mystery(9) = Mystery(7) + Mystery(8)</div>
+            <div>從Mystery(1)反推回去</div>
+            <div>Mystery(1) = 1</div>
+            <div>Mystery(2) = 1</div>
+            <div>Mystery(3) = 2</div>
+            <div>Mystery(4) = 3</div>
+            <div>Mystery(5) = 5</div>
+            <div>Mystery(6) = 8</div>
+            <div>Mystery(7) = 13</div>
+            <div>Mystery(8) = 21</div>
+            <div>Mystery(9) = 34</div>
+            <div>Mystery(10) = 55</div>
+            <div>Mystery(11) = 89</div>
+            <div>
+              (C) Mystery(x-2) + Mystery(x+2) = Mystery(7) + Mystery(11) = 13 +
+              89 = 102
+            </div>
+            <div>
+              (D) Mystery(x-2) + Mystery(x-1) = Mystery(7) + Mystery(8) = 13 +
+              21 = 34
+            </div>
+            <div>故answer = (D)</div>
+          </div>
+        </div>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
-
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      date: "2016-03-05",
+      image: {
+        description: require("@/assets/problems/concept/2016-03-05/25_description.png"),
+        code: require("@/assets/problems/concept/2016-03-05/25_code.png")
+      },
+      answerGroup: [],
+      bShowAnswer: false
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
