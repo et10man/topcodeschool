70e2c3c28b6f1738e5bf8a66d01b08898c6e941b
james-yl.tsai <jameslovee10@gmail.com>
2019-05-03 00:53:58 +0800

    add Concept_Problem_2016_03_05_22.vue

--- src/views/courses/apcs/archive/concept/2016-03-05/Concept_Problem_2016_03_05_22.vue
+++ src/views/courses/apcs/archive/concept/2016-03-05/Concept_Problem_2016_03_05_22.vue
@@ -1,11 +1,51 @@
 <template>
   <v-container>
-    <h1>Problem-22</h1>
+    <v-layout row wrap justify-center>
+      <v-flex xs5>
+        <v-img :src="image.description"></v-img>
+      </v-flex>
+      <v-flex xs5>
+        <v-img :src="image.code"></v-img>
+      </v-flex>
+      <v-flex xs2></v-flex>
+      <v-flex xs4>
+        <v-radio-group v-model="answerGroup">
+          <v-radio label="1023" color="error" value="error_1"></v-radio>
+          <v-radio label="1024" color="error" value="error_2"></v-radio>
+          <v-radio label="2047" color="error" value="error_3"></v-radio>
+          <v-radio label="2048" color="success" value="success_4"></v-radio>
+        </v-radio-group>
+      </v-flex>
+      <v-flex xs8>
+        <v-btn color="info" @click="bShowAnswer = !bShowAnswer">
+          {{ bShowAnswer ? "隱藏詳解" : "顯示詳解" }}
+        </v-btn>
+        <div v-if="bShowAnswer">
+          <h2>解答:</h2>
+          <div class="subheading">
+            <div>2的十次方=1024, 2的十一次方=2048</div>
+            <div>2048時while(p < 2000)才不成立</div>
+            <div>故answer = 2048</div>
+          </div>
+        </div>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
-
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      date: "2016-03-05",
+      image: {
+        description: require("@/assets/problems/concept/2016-03-05/22_description.png"),
+        code: require("@/assets/problems/concept/2016-03-05/22_code.png")
+      },
+      answerGroup: [],
+      bShowAnswer: false
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
