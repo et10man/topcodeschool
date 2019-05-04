bf688939d9658785f2ad23315cc534b36d4f8e1c
james-yl.tsai <jameslovee10@gmail.com>
2019-05-04 15:16:35 +0800

    add Concept_Problem_2016_10_29_9.vue

--- src/views/courses/apcs/archive/concept/2016-10-29/Concept_Problem_2016_10_29_9.vue
+++ src/views/courses/apcs/archive/concept/2016-10-29/Concept_Problem_2016_10_29_9.vue
@@ -1,11 +1,55 @@
 <template>
   <v-container>
-    <h1>Problem-9</h1>
+    <v-layout row wrap justify-center>
+      <v-flex xs5>
+        <v-img :src="image.description"></v-img>
+      </v-flex>
+      <v-flex xs5>
+        <v-img :src="image.code"></v-img>
+      </v-flex>
+      <v-flex xs2></v-flex>
+      <v-flex xs4>
+        <v-radio-group v-model="answerGroup">
+          <v-radio label="20" color="error" value="error_1"></v-radio>
+          <v-radio label="11" color="success" value="success_2"></v-radio>
+          <v-radio label="2" color="error" value="error_3"></v-radio>
+          <v-radio label="10" color="error" value="error_4"></v-radio>
+        </v-radio-group>
+      </v-flex>
+      <v-flex xs8>
+        <v-btn color="info" @click="bShowAnswer = !bShowAnswer">{{
+          bShowAnswer ? "隱藏詳解" : "顯示詳解"
+        }}</v-btn>
+        <div v-if="bShowAnswer">
+          <h2>解答:</h2>
+          <div class="subheading">
+            <div>s等於60,70,71,72,73,74,75,76,77,78,79時</div>
+            <div>分數的等第是錯的</div>
+            <div>故answer = 11</div>
+          </div>
+        </div>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
-
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      date: "2016-10-29",
+      image: {
+        description: require("@/assets/problems/concept/2016-10-29/9_description.png"),
+        code: require("@/assets/problems/concept/2016-10-29/9_code.png")
+      },
+      answerGroup: [],
+      bShowAnswer: false
+    };
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.no-dot {
+  list-style-type: none;
+}
+</style>
