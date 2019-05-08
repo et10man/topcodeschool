74fc830403d4104bd3b2dce7250063a118a44a49
james-yl.tsai <jameslovee10@gmail.com>
2019-05-08 10:49:28 +0800

    add Concept_Problem_2016_10_29_14.vue

--- src/views/courses/apcs/archive/concept/2016-10-29/Concept_Problem_2016_10_29_14.vue
+++ src/views/courses/apcs/archive/concept/2016-10-29/Concept_Problem_2016_10_29_14.vue
@@ -1,11 +1,79 @@
 <template>
   <v-container>
-    <h1>Problem-14</h1>
+    <v-layout row wrap justify-center>
+      <v-flex xs12>
+        <v-img :src="image.description"></v-img>
+      </v-flex>
+      <v-flex xs4>
+        <v-radio-group v-model="answerGroup">
+          <v-radio
+            label="TRUE FALSE TRUE FALSE"
+            color="success"
+            value="success_1"
+          ></v-radio>
+          <v-radio
+            label="FALSE FALSE TRUE FALSE"
+            color="error"
+            value="error_2"
+          ></v-radio>
+          <v-radio
+            label="FALSE TRUE TRUE FALSE"
+            color="error"
+            value="error_3"
+          ></v-radio>
+          <v-radio
+            label="TRUE TRUE FALSE TRUE"
+            color="error"
+            value="error_4"
+          ></v-radio>
+        </v-radio-group>
+      </v-flex>
+      <v-flex xs8>
+        <v-btn color="info" @click="bShowAnswer = !bShowAnswer">{{
+          bShowAnswer ? "隱藏詳解" : "顯示詳解"
+        }}</v-btn>
+        <div v-if="bShowAnswer">
+          <h2>解答:</h2>
+          <div class="subheading">
+            <div>x=TRUE, y=TRUE, z=FALSE</div>
+            <div>
+              !(y || z) || x = !(TRUE || FALSE) || TRUE = FALSE || TRUE = TRUE
+            </div>
+            <div>
+              !y || (z || !x) = !TRUE || (FALSE || !TRUE) = FALSE || FALSE =
+              FALSE
+            </div>
+            <div>
+              z || (x && (y || z)) = FALSE || (TRUE && (TRUE || FALSE)) = FALSE
+              || TRUE = TRUE
+            </div>
+            <div>
+              (x || x) && z = (TRUE || TRUE) && FALSE = TRUE && FALSE = FALSE
+            </div>
+            <div>故answer = TRUE FALSE TRUE FALSE</div>
+          </div>
+        </div>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
-
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      date: "2016-10-29",
+      image: {
+        description: require("@/assets/problems/concept/2016-10-29/14_description.png")
+      },
+      answerGroup: [],
+      bShowAnswer: false
+    };
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.no-dot {
+  list-style-type: none;
+}
+</style>
