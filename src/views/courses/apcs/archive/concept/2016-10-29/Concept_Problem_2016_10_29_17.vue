15d87a7a9be1c26f822c82b4c8b4913cdd5a4acc
james-yl.tsai <jameslovee10@gmail.com>
2019-05-08 14:52:26 +0800

    add Concept_Problem_2016_10_29_17.vue

--- src/views/courses/apcs/archive/concept/2016-10-29/Concept_Problem_2016_10_29_17.vue
+++ src/views/courses/apcs/archive/concept/2016-10-29/Concept_Problem_2016_10_29_17.vue
@@ -1,11 +1,57 @@
 <template>
   <v-container>
-    <h1>Problem-17</h1>
+    <v-layout row wrap justify-center>
+      <v-flex xs5>
+        <v-img :src="image.description"></v-img>
+      </v-flex>
+      <v-flex xs5>
+        <v-img :src="image.code"></v-img>
+      </v-flex>
+      <v-flex xs2></v-flex>
+      <v-flex xs4>
+        <v-radio-group v-model="answerGroup">
+          <v-radio label="k > 2" color="error" value="error_1"></v-radio>
+          <v-radio label="k > 1" color="error" value="error_2"></v-radio>
+          <v-radio label="k > 0" color="success" value="success_3"></v-radio>
+          <v-radio label="k > －1" color="error" value="error_4"></v-radio>
+        </v-radio-group>
+      </v-flex>
+      <v-flex xs8>
+        <v-btn color="info" @click="bShowAnswer = !bShowAnswer">{{
+          bShowAnswer ? "隱藏詳解" : "顯示詳解"
+        }}</v-btn>
+        <div v-if="bShowAnswer">
+          <h2>解答:</h2>
+          <div class="subheading">
+            <div>*個數分別為6, 4, 2, 0</div>
+            <div>for (int k=6-2*i; (a) ; k=k-1)</div>
+            <div>(a) k > 0時</div>
+            <div>符合*個數6, 4, 2, 0</div>
+            <div>故answer = (a) k > 0時</div>
+          </div>
+        </div>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
-
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      date: "2016-10-29",
+      image: {
+        description: require("@/assets/problems/concept/2016-10-29/17_description.png"),
+        code: require("@/assets/problems/concept/2016-10-29/17_code.png")
+      },
+      answerGroup: [],
+      bShowAnswer: false
+    };
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.no-dot {
+  list-style-type: none;
+}
+</style>
