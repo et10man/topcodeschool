52429779729165af586eb4f05d96f9a66854c1d7
james-yl.tsai <jameslovee10@gmail.com>
2019-05-08 15:53:37 +0800

    add Concept_Problem_2016_10_29_20.vue

--- src/views/courses/apcs/archive/concept/2016-10-29/Concept_Problem_2016_10_29_20.vue
+++ src/views/courses/apcs/archive/concept/2016-10-29/Concept_Problem_2016_10_29_20.vue
@@ -10,26 +10,10 @@
       <v-flex xs2></v-flex>
       <v-flex xs4>
         <v-radio-group v-model="answerGroup">
-          <v-radio
-            label="冗餘程式碼在 A 區"
-            color="error"
-            value="error_1"
-          ></v-radio>
-          <v-radio
-            label="冗餘程式碼在 B 區"
-            color="error"
-            value="error_2"
-          ></v-radio>
-          <v-radio
-            label="冗餘程式碼在 C 區"
-            color="error"
-            value="error_3"
-          ></v-radio>
-          <v-radio
-            label="冗餘程式碼在 D 區"
-            color="success"
-            value="success_4"
-          ></v-radio>
+          <v-radio label="0" color="error" value="error_1"></v-radio>
+          <v-radio label="10" color="error" value="error_2"></v-radio>
+          <v-radio label="25" color="error" value="error_3"></v-radio>
+          <v-radio label="50" color="success" value="success_4"></v-radio>
         </v-radio-group>
       </v-flex>
       <v-flex xs8>
@@ -39,13 +23,9 @@
         <div v-if="bShowAnswer">
           <h2>解答:</h2>
           <div class="subheading">
-            <div>G(1)</div>
-            <div>= 印出1 進入G(2)</div>
-            <div>= 印出2 進入G(3)</div>
-            <div>= 印出3 return</div>
-            <div>= 回到G(2) 印出2</div>
-            <div>= 回到G(1) 印出1</div>
-            <div>故answer = 1 2 3 2 1</div>
+            <div>A = G(5) = 25</div>
+            <div>A = G(m) + A = G(5) + 25 = 25 + 25 = 50</div>
+            <div>故answer = 50</div>
           </div>
         </div>
       </v-flex>
@@ -58,8 +38,8 @@ export default {
     return {
       date: "2016-10-29",
       image: {
-        description: require("@/assets/problems/concept/2016-10-29/19_description.png"),
-        code: require("@/assets/problems/concept/2016-10-29/19_code.png")
+        description: require("@/assets/problems/concept/2016-10-29/20_description.png"),
+        code: require("@/assets/problems/concept/2016-10-29/20_code.png")
       },
       answerGroup: [],
       bShowAnswer: false
