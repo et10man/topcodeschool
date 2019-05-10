437005949f2f325d94e76315d1c67c50c1355c61
james-yl.tsai <jameslovee10@gmail.com>
2019-05-10 11:30:00 +0800

    add Concept_Problem_2016_10_29_24.vue

--- src/views/courses/apcs/archive/concept/2016-10-29/Concept_Problem_2016_10_29_24.vue
+++ src/views/courses/apcs/archive/concept/2016-10-29/Concept_Problem_2016_10_29_24.vue
@@ -1,11 +1,63 @@
 <template>
   <v-container>
-    <h1>Problem-24</h1>
+    <v-layout row wrap justify-center>
+      <v-flex xs5>
+        <v-img :src="image.description"></v-img>
+      </v-flex>
+      <v-flex xs5>
+        <v-img :src="image.code"></v-img>
+      </v-flex>
+      <v-flex xs2></v-flex>
+      <v-flex xs4>
+        <v-radio-group v-model="answerGroup">
+          <v-radio label="128" color="error" value="error_1"></v-radio>
+          <v-radio label="2187" color="success" value="success_2"></v-radio>
+          <v-radio label="6561" color="error" value="error_3"></v-radio>
+          <v-radio label="1024" color="error" value="error_4"></v-radio>
+        </v-radio-group>
+      </v-flex>
+      <v-flex xs8>
+        <v-btn color="info" @click="bShowAnswer = !bShowAnswer">{{
+          bShowAnswer ? "隱藏詳解" : "顯示詳解"
+        }}</v-btn>
+        <div v-if="bShowAnswer">
+          <h2>解答:</h2>
+          <div class="subheading">
+            <div>G(3, 7)</div>
+            <div>= 3 * G(3, 6)</div>
+            <div>= 3 * 3 * G(3, 5)</div>
+            <div>= 3 * 3 * 3 * G(3, 4)</div>
+            <div>= 3 * 3 * 3 * 3 * G(3, 3)</div>
+            <div>= 3 * 3 * 3 * 3 * 3 * G(3, 2)</div>
+            <div>= 3 * 3 * 3 * 3 * 3 * 3 * G(3, 1)</div>
+            <div>= 3 * 3 * 3 * 3 * 3 * 3 * 3 * G(3, 0)</div>
+            <div>= 3 * 3 * 3 * 3 * 3 * 3 * 3 * 1</div>
+            <div>= 3^7 = 2187</div>
+            <div>故answer = 2187</div>
+          </div>
+        </div>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
-
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      date: "2016-10-29",
+      image: {
+        description: require("@/assets/problems/concept/2016-10-29/24_description.png"),
+        code: require("@/assets/problems/concept/2016-10-29/24_code.png")
+      },
+      answerGroup: [],
+      bShowAnswer: false
+    };
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.no-dot {
+  list-style-type: none;
+}
+</style>
