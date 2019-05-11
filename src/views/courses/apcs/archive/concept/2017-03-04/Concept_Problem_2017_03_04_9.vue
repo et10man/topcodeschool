3f316296415b48b81c9485e592a28aca66321db6
james-yl.tsai <jameslovee10@gmail.com>
2019-05-11 13:49:10 +0800

    add Concept_Problem_2017_03_04_9.vue

--- src/views/courses/apcs/archive/concept/2017-03-04/Concept_Problem_2017_03_04_9.vue
+++ src/views/courses/apcs/archive/concept/2017-03-04/Concept_Problem_2017_03_04_9.vue
@@ -1,11 +1,73 @@
 <template>
   <v-container>
-    <h1>Problem-9</h1>
+    <v-layout row wrap justify-center>
+      <v-flex xs5>
+        <v-img :src="image.description"></v-img>
+      </v-flex>
+      <v-flex xs5>
+        <v-img :src="image.code"></v-img>
+      </v-flex>
+      <v-flex xs2></v-flex>
+      <v-flex xs4>
+        <v-radio-group v-model="answerGroup">
+          <v-radio
+            label="0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+            color="error"
+            value="error_1"
+          ></v-radio>
+          <v-radio
+            label="2, 0, 2, 0, 2, 0, 2, 0, 2, 0"
+            color="error"
+            value="error_2"
+          ></v-radio>
+          <v-radio
+            label="9, 0, 1, 2, 3, 4, 5, 6, 7, 8"
+            color="error"
+            value="error_3"
+          ></v-radio>
+          <v-radio
+            label="8, 9, 0, 1, 2, 3, 4, 5, 6, 7"
+            color="success"
+            value="success_4"
+          ></v-radio>
+        </v-radio-group>
+      </v-flex>
+      <v-flex xs8>
+        <v-btn color="info" @click="bShowAnswer = !bShowAnswer">
+          {{ bShowAnswer ? "隱藏詳解" : "顯示詳解" }}
+        </v-btn>
+        <div v-if="bShowAnswer">
+          <h2>解答:</h2>
+          <div class="subheading">
+            <div>scanf("%d", &X[(i+2)%10])</div>
+            <div>X填的位置依序為2,3,4,5,6,7,8,9,0,1</div>
+            <div>X填的整數依序為0,1,2,3,4,5,6,7,8,9</div>
+            <div>X = {8, 9, 0, 1, 2, 3, 4, 5, 6, 7}</div>
+            <div>故answer = 8, 9, 0, 1, 2, 3, 4, 5, 6, 7</div>
+          </div>
+        </div>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
-
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      date: "2017-03-04",
+      image: {
+        description: require("@/assets/problems/concept/2017-03-04/9_description.png"),
+        code: require("@/assets/problems/concept/2017-03-04/9_code.png")
+      },
+      answerGroup: [],
+      bShowAnswer: false
+    };
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.no-dot {
+  list-style-type: none;
+}
+</style>
