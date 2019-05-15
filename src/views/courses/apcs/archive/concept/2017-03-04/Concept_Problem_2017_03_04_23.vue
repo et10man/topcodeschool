527e46880b38534538c97d1e6ab2fd5173eb54fb
james-yl.tsai <jameslovee10@gmail.com>
2019-05-15 14:58:34 +0800

    add Concept_Problem_2017_03_04_23.vue

--- src/views/courses/apcs/archive/concept/2017-03-04/Concept_Problem_2017_03_04_23.vue
+++ src/views/courses/apcs/archive/concept/2017-03-04/Concept_Problem_2017_03_04_23.vue
@@ -1,11 +1,49 @@
 <template>
   <v-container>
-    <h1>Problem-23</h1>
+    <v-layout row wrap justify-center>
+      <v-flex xs12>
+        <v-img :src="image.description"></v-img>
+      </v-flex>
+      <v-flex xs4>
+        <v-radio-group v-model="answerGroup">
+          <v-radio label="記憶體" color="success" value="success_1"></v-radio>
+          <v-radio label="硬碟" color="error" value="error_2"></v-radio>
+          <v-radio label="輸出入裝置" color="error" value="error_3"></v-radio>
+          <v-radio label="匯流排" color="error" value="error_4"></v-radio>
+        </v-radio-group>
+      </v-flex>
+      <v-flex xs8>
+        <v-btn color="info" @click="bShowAnswer = !bShowAnswer">{{
+          bShowAnswer ? "隱藏詳解" : "顯示詳解"
+        }}</v-btn>
+        <div v-if="bShowAnswer">
+          <h2>解答:</h2>
+          <div class="subheading">
+            <div>CPU會將硬碟上的程式碼load到記憶體執行</div>
+            <div>故answer = 記憶體</div>
+          </div>
+        </div>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
-
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      date: "2017-03-04",
+      image: {
+        description: require("@/assets/problems/concept/2017-03-04/23_description.png")
+      },
+      answerGroup: [],
+      bShowAnswer: false
+    };
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.no-dot {
+  list-style-type: none;
+}
+</style>
